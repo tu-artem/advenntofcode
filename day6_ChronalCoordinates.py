3a33c4f8e912871a7a908b0ea033c061e4535d47
Artem <tu_artem@mail.ru>
2018-12-06 16:59:15 +0200

    stupid code removed

--- day6_ChronalCoordinates.py
+++ day6_ChronalCoordinates.py
@@ -21,20 +21,11 @@ def find_boundaries(coords: List[Coord]) -> Tuple[Coord, Coord]:
     """Scans all the input corrdinanes and returns Coord_hi and Coord_lo
     that defines the boundary around all the input coords
     """
-    x_hi, y_hi = coords[0]
-    x_lo, y_lo = coords[1]
-    for coord in coords:
-        x, y = coord
-
-        if x < x_hi:
-            x_hi = x
-        if x > x_lo:
-            x_lo = x
-        if y < y_hi:
-            y_hi = y
-        if y > y_lo:
-            y_lo = y
-    
+    x_hi = min(coord.x for coord in coords)
+    y_hi = min(coord.y for coord in coords)
+    x_lo = max(coord.x for coord in coords)
+    y_lo = max(coord.y for coord in coords)
+   
     return Coord(x_hi, y_hi), Coord(x_lo, y_lo)
 
 def distance(c1: Coord, c2: Coord) -> int:
@@ -99,15 +90,15 @@ def safe_distance(coords: List[Coord], threshold: int = 32) -> int:
 
             
 
-# print(get_neighbours(coords))
+print(get_neighbours(coords))
 
 with open("day6_input1.txt", "r") as f:
     coords_str = [line.strip() for line in f]
 
 
 coords = parse_coords(coords_str)
-#neighbours = get_neighbours(coords)
+neighbours = get_neighbours(coords)
 
-# print(max(neighbours.values()))
+print(max(neighbours.values()))
 
 print(safe_distance(coords, threshold=10000))
