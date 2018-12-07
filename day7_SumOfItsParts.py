bb1fde29e78c2f197f2f2544e062081bf66734b7
Artem <tu_artem@mail.ru>
2018-12-07 19:37:42 +0200

    day7 part1

--- day7_SumOfItsParts.py
+++ day7_SumOfItsParts.py
@@ -4,7 +4,7 @@ https://adventofcode.com/2018/day/7
 
 
 import re
-from typing import NamedTuple, List, Tuple
+from typing import NamedTuple, List, Tuple, Dict
 
 r"""
   -->A--->B--
@@ -38,18 +38,85 @@ class Edge(NamedTuple):
 
 edges = [Edge.from_str(x.strip()) for x in TEST_CASE.split("\n") if x]
 
+def build_graph(edges: List[Edge], reverse=False) -> Dict[str, List[str]]:
+    graph: Dict[str, List[str]] = {}
+    for edge in edges:
+        if not reverse:
+            fr = edge.fr
+            to = edge.to
+        else:
+            fr = edge.to
+            to = edge.fr
+        if fr in graph:
+            graph[fr].append(to)
+        else:
+            graph[fr] = [to]
+    return graph
 
 
 
+def find_start_end(edges: List[Edge]) -> Tuple[List[str], List[str]]:
+    fr, to = zip(*[(edge.fr, edge.to) for edge in edges])
+    start = [x for x in fr if x not in to]
+    end = [x for x in to if x not in fr]
 
-candidates = []
-executed = {}
+    return set(start), set(end)
+
+graph = build_graph(edges)
+
+rev_graph =build_graph(edges, True)
+start, end = find_start_end(edges)
 
 
-print(edges)
+def find_path(graph, rev_graph, start, end):
+    candidates = list(start)
+    
+    def check_all(l1, l2):
+        return all([(x in l2) for x in l1])
+    
+    executed = []
+    while True:
+        
+        to_execute = min(candidates)
+        
+        parents = rev_graph.get(to_execute)
+        executed.append(to_execute)
+        if parents:
+            all_parents = check_all(parents, executed)  
+            if not all_parents:
+                candidates.remove(to_execute)
+                executed.remove(to_execute)
+                continue
+        
+        children = graph.get(to_execute)
+        if children:
+            new_candidates = [x for x in children if x not in executed]
+           
+            candidates.extend(new_candidates)
+            candidates = [x for x in candidates if x not in executed]
+          
+        reach_end = check_all(end, executed)
+        if reach_end:
+            return "".join(executed)
+        
+
+
+    
+    
+
+
+find_path(graph,rev_graph, start, end)
+
+
 
 with open("day7_input.txt") as f:
     lines = [line.strip() for line in f]
 
 edges = [Edge.from_str(x.strip()) for x in lines]
 
+graph = build_graph(edges)
+
+rev_graph =build_graph(edges, True)
+start, end = find_start_end(edges)
+
+print(find_path(graph,rev_graph, start, end))
