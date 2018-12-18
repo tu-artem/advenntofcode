ff2625a4e252deafba1f2095b0de8157e292f350
Artem <tu_artem@mail.ru>
2018-12-18 12:11:57 +0200

    day17 finally finished

--- day17_ReservoirResearch.py
+++ day17_ReservoirResearch.py
@@ -17,6 +17,7 @@ y=7, x=495..501
 x=501, y=3..7
 x=498, y=2..4
 x=506, y=1..2
+x=502, y=11
 x=498, y=10..13
 x=504, y=10..13
 y=13, x=498..504"""
@@ -37,15 +38,15 @@ def from_line(line: str) -> List[Clay]:
 
 LINES = RAW.split("\n")
 
-CLAY = []
+CLAYS = []
 
 for line in LINES:
-    CLAY.extend(from_line(line))
+    CLAYS.extend(from_line(line))
 
 
 SPGING = (500, 0)
 
-def plot_clay(clays: List[Clay], water: List[Water]) -> str:
+def plot_clay(clays: List[Clay], water: List[Water], pouring_water: List[Water]) -> str:
     x_min = min([clay[0] for clay in clays])
     x_max = max([clay[0] for clay in clays])
     y_min = min([clay[1] for clay in clays])
@@ -53,7 +54,7 @@ def plot_clay(clays: List[Clay], water: List[Water]) -> str:
 
     water = set(water)
     clays = set(clays)
-
+    pouring_water = set(pouring_water)
     grid = []
 
     for y in range(y_min - 1, y_max+1):
@@ -62,6 +63,8 @@ def plot_clay(clays: List[Clay], water: List[Water]) -> str:
                 grid.append("+")
             elif (x, y) in water:
                 grid.append("~")
+            elif (x, y) in pouring_water:
+                grid.append("|")
             elif (x, y) in clays:
                 grid.append("#")
             else:
@@ -110,11 +113,15 @@ def fill_up(left, right):
 def waterfall(current: Tuple[int, int], clays: List[Clay], water=[]):
     print(current)
     water = water[:]
+    pouring_water = []
     try:
         hit = hit_ground(current, clays, water)
      #   print("hit: ", hit)
     except ValueError:
-        return []
+        return water, [], [(current[0], y) for y in range(current[1] + 1, 1 + max([clay[1] for clay in clays]))]
+
+    ## Count pouring water
+    pouring_water.extend([(current[0], y) for y in range(current[1] + 1, hit[1])])
 
     new_sources = []
 
@@ -126,7 +133,8 @@ def waterfall(current: Tuple[int, int], clays: List[Clay], water=[]):
                 # print(f"ready to move down from right {to_the_right}")
                 # print([Clay(x,y) for (x,y) in clays.union(all_water)],  clays.union(all_water))
                 new_sources.append(to_the_right)
-                return None
+                pouring_water.extend([(x, to_the_right[1]) for x in range(position[0], to_the_right[0]+1)])
+                return False
             elif to_the_right in clays:
                 return to_the_right
             to_the_right = settle_right(to_the_right)
@@ -138,7 +146,9 @@ def waterfall(current: Tuple[int, int], clays: List[Clay], water=[]):
             if not clay_below(to_the_left, clays, water):
                 # print("ready to move down from left")
                 new_sources.append(to_the_left)
-                return None
+                pouring_water.extend([(x, to_the_left[1]) for x in range(position[0], to_the_left[0]-1, -1)])
+             
+                return False
             elif to_the_left in clays:
                 return to_the_left
             to_the_left = settle_left(to_the_left)
@@ -147,35 +157,45 @@ def waterfall(current: Tuple[int, int], clays: List[Clay], water=[]):
     
     right = find_right(hit)
     left = find_left(hit)
+
+    if left and not right:
+        pouring_water.extend([(x, left[1]) for x in range(left[0]+1, hit[0])])
     
+    if right and not left:
+        pouring_water.extend([(x, right[1]) for x in range(hit[0]+1, right[0])])
+
     while right and left:
         water.extend(fill_up(left, right))
         hit = hit[0], hit[1] - 1
         right = find_right(hit)
-        left = find_left(hit)
-    return water, new_sources
+        left = find_left(hit)  
+    if left and not right:
+        pouring_water.extend([(x, left[1]) for x in range(left[0]+1, hit[0])])
+    if right and not left:
+        pouring_water.extend([(x, right[1]) for x in range(hit[0]+1, right[0])])
+    return water, new_sources, pouring_water
 
 
 
 
 def flow(clays: List[Clay], show=False):
 
-    water, sources = waterfall((500, 0), clays, [])
-
+    water, sources, pouring_water = waterfall((500, 0), clays, [])
     sources = deque(sources)
+    passed_sources = set()
+    
+    while len(sources) > 0:
+    #     print("Settled:", len(water), len(set(water)))
+    #     print("Pouring:", len(pouring_water), len(set(pouring_water)))
+        curr = sources.pop()
+        passed_sources.add(curr)
+        water, s, pw = waterfall(curr, clays, water)
+        pouring_water.extend(pw)
+        sources.extend([sor for sor in s if sor not in passed_sources])
+        if show:
+            print(plot_clay(clays, water, pouring_water))
 
-    while True:
-        print(len(water), len(set(water)))
-        curr = sources.popleft()
-        res = waterfall(curr, clays, water)
-        if res:
-            water, s = res
-            sources.extend(s)
-            if show:
-                print(plot_clay(clays, water))
-        else:
-            break
-    return water
+    return water,pouring_water,  sources, passed_sources
 
 
 # w, s = waterfall((500, 0), CLAY, [])
@@ -185,14 +205,36 @@ with open("day17_input.txt") as f:
     lines = [line.strip() for line in f]
 
 
-claysss = []
+clays = []
 
 for line in lines:
-    claysss.extend(from_line(line))
+    clays.extend(from_line(line))
 
 # print(plot_clay(clays, []))
 
 
-water = flow(claysss)
+# water,pouring_water, sources, passed = flow(CLAYS)
+# print(plot_clay(CLAYS, water, pouring_water))
 
-# print(plot_clay(clays, water))
+
+
+# assert len(set(chain(water, pouring_water))) == 57
+
+
+water,pouring_water, sources, passed = flow(clays)
+print(plot_clay(clays, water, pouring_water))
+
+ans = set(chain(water, pouring_water))
+
+
+y_min = min([clay[1] for clay in clays])
+y_max = max([clay[1] for clay in clays])
+
+
+aaa = {a for a in ans if a[1] >= y_min and a[1]<=y_max}
+len(aaa)
+
+
+
+### Part 2
+print(len(set(water)))
